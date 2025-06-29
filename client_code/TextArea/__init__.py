74f1df266794aae9ebb6cb528606668a911ffc1f
divyeshlakhotia@gmail.com <divyeshlakhotia@gmail.com>
2025-06-29 18:11:01 +0000

    Edited TextBox and TextArea

--- client_code/TextArea/__init__.py
+++ client_code/TextArea/__init__.py
@@ -3,10 +3,15 @@ from .. import SuperComponent
 
 class TextArea(TextAreaTemplate):
     def __init__(self, **properties):
-        self.super_comp = SuperComponent.SuperComponent(self, events = ["hover", "hover_out", "focus", "lost_focus", "input", "change"], **properties)
+        self.super_comp = SuperComponent.SuperComponent(self, events = ["hover", "hover_out", "focus", "lost_focus", "change"], **properties)
         self.is_textarea = True
         self.init_components(**properties)
+        self.add_event("input", self._set_text_on_input)
 
+    def _set_text_on_input(self, **event_args):
+        event_args['sender'].text = event_args['event'].target.value
+        self.raise_event("input")
+        
     def __getattr__(self, name):
         try:
             return object.__getattribute__(self, name)
--- client_code/TextBox/__init__.py
+++ client_code/TextBox/__init__.py
@@ -1,14 +1,19 @@
 from ._anvil_designer import TextBoxTemplate
 from .. import SuperComponent
-
+from anvil import *
 class TextBox(TextBoxTemplate):
     def __init__(self, **properties):
-        self.super_comp = SuperComponent.SuperComponent(self, events = ["hover", "hover_out", "focus", "lost_focus", "input", "change"], **properties)
+        self.super_comp = SuperComponent.SuperComponent(self, events = ["hover", "hover_out", "focus", "lost_focus", "change"], **properties)
         self.is_textbox = True
+        self.add_event("input", self._set_text_on_input)
         self.init_components(**properties)
-
         self.add_event("keydown", self._detect_enter_press)
+        
 
+    def _set_text_on_input(self, **event_args):
+        self.text = event_args['event'].target.value
+        self.raise_event("input")
+        
     def _detect_enter_press(self, **event_args):
         if event_args['event'].key == "Enter":
             self.raise_event("pressed_enter")
