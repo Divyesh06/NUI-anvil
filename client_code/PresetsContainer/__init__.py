b3d5e14f10701905bc03ef9e180ad221b178d4ed
divyeshlakhotia@gmail.com <divyeshlakhotia@gmail.com>
2025-10-27 15:24:30 +0000

    Edited PresetsContainer

--- client_code/PresetsContainer/__init__.py
+++ client_code/PresetsContainer/__init__.py
@@ -1,9 +1,11 @@
 from ._anvil_designer import PresetsContainerTemplate
 from anvil.designer import in_designer
-
+from anvil.js.window import document
 
 class PresetsContainer(PresetsContainerTemplate):
     def __init__(self, **properties):
         self.init_components(**properties)
         if not in_designer:
             self.visible = False
+        else:
+            document.body.style.marginTop = "50px"
