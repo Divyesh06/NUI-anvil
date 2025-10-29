72b94c7e067f65b11b6dccfa4d5354b0da205e2d
divyeshlakhotia@gmail.com <divyeshlakhotia@gmail.com>
2025-10-29 12:57:53 +0000

    Created Link

---
+++ client_code/Link/__init__.py
@@ -0,0 +1,19 @@
+from ._anvil_designer import LinkTemplate
+from .. import SuperComponent
+
+class Link(LinkTemplate):
+    def __init__(self, **properties):
+        self.super_comp = SuperComponent.SuperComponent(self, events = ["hover", "hover_out", "click"], **properties)
+        self.remove_from_parent = self.super_comp.remove_from_parent
+        self.init_components(**properties)
+
+    def __getattr__(self, name):
+        try:
+            return object.__getattribute__(self, name)
+        except AttributeError:
+            super_comp = object.__getattribute__(self, "super_comp")
+            return getattr(super_comp, name)
+
+    def __setattr__(self, name, value):
+        object.__setattr__(self, name, value)
+        setattr(self.super_comp, name, value)
