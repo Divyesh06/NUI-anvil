dc380b0d8af898c99739dcbf68f85694a6827c29
divyeshlakhotia@gmail.com <divyeshlakhotia@gmail.com>
2025-11-11 10:04:16 +0000

    Edited settings, Form2 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,9 @@
 from ._anvil_designer import Form1Template
-
+from ..Form2 import Form2
 
 class Form1(Form1Template):
     def __init__(self, **properties):
         self.init_components(**properties)
+        a = Form2()
+        a.container_1.remove_from_parent()
+        self.container_1.add_component(a.container_1)
